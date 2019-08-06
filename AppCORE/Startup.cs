5fd63133f1607a2e24c9f27428fa6aef9949bd4e
Denis Sidorov <dsigmat@hotmail.com>
2019-08-06 15:01:20 +0600

    https://www.youtube.com/watch?v=hbFPnPbIUcw time 35:30

--- AppCORE/Startup.cs
+++ AppCORE/Startup.cs
@@ -15,19 +15,51 @@ namespace AppCORE
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var a = 1;
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    a = a + 1;
+            //    await context.Response.WriteAsync($"USE a = {a}");
+
+            //    await next();
+
+            //    //await context.Response.WriteAsync($" Result = {a}");
+
+            //});
+
+            //MAP
+            app.Map("/plus", (appBuilder) =>
+            {
+                appBuilder.Run(async (context) =>
+                {
+                    a = a + 2;
+                    await context.Response.WriteAsync($" Map a = {a}");
+                });
+            });
+            app.Map("/min", (appBuilder) =>
+            {
+                appBuilder.Run(async (context) =>
+                {
+                    a = a - 2;
+                    await context.Response.WriteAsync($" Map a = {a}");
+                });
+            });
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                a = a * 2;
+                await context.Response.WriteAsync($", run a = {a}");
             });
         }
     }
---
+++ AppCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
